4ca3dc33991b30317bed5b5a4fa1943a67284da5
jerlyjelly <68527525+jerlyjelly@users.noreply.github.com>
2021-07-12 00:35:17 +0900

    added non functioning tab bar

--- lib/screens/students/students_detail.dart
+++ lib/screens/students/students_detail.dart
@@ -9,34 +9,55 @@ class StudentsDetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: SingleChildScrollView(
-        padding: EdgeInsets.all(defaultPadding),
-        child: Column(
-          children: [
-            StudentsHeader(),
-            SizedBox(height: defaultPadding),
-            Row(
+      child: DefaultTabController(
+        //$ change length to # of teams managed by a teacher
+        length: 3,
+        child: Scaffold(
+          body: SingleChildScrollView(
+            padding: EdgeInsets.all(defaultPadding),
+            child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Expanded(
-                  flex: 5,
-                  child: Column(
-                    children: [
-                      StudentsList(),
-                      if (Responsive.isMobile(context)) SizedBox(height: defaultPadding),
-                    ],
-                  ),
+                StudentsHeader(),
+                SizedBox(height: defaultPadding),
+                TabBar(
+                  onTap: (index) {
+                    print(index);
+                  },
+                  isScrollable: true,
+                  // labelStyle: boldTextStyle(),
+                  indicatorColor: Colors.white,
+                  tabs: [
+                    Tab(
+                      child: Text('All'),
+                    ),
+                    Tab(
+                      child: Text('Team 1'),
+                    ),
+                    Tab(
+                      child: Text('Team 2'),
+                    ),
+                  ],
                 ),
-                if (!Responsive.isMobile(context)) SizedBox(width: defaultPadding),
-                // On Mobile means if the screen is less than 850 we dont want to show it
-                // if (!Responsive.isMobile(context))
-                //   Expanded(
-                //     flex: 2,
-                //     child: StarageDetails(),
-                //   ),
+                SizedBox(height: defaultPadding),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Expanded(
+                      flex: 5,
+                      child: Column(
+                        children: [
+                          StudentsList(),
+                          if (Responsive.isMobile(context)) SizedBox(height: defaultPadding),
+                        ],
+                      ),
+                    ),
+                    if (!Responsive.isMobile(context)) SizedBox(width: defaultPadding),
+                  ],
+                )
               ],
-            )
-          ],
+            ),
+          ),
         ),
       ),
     );
